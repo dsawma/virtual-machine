5f3c126f38d104eea98baefe271b394eb80c66e9
dsawma <davidthanga13@gmail.com>
2026-04-05 21:52:23 -0400

    3) Fetch and decode

--- code.c
+++ code.c
@@ -40,9 +40,72 @@ enum{
     OP_TRAP     /* execute trap */
 };
 
+/* 001 == pos, 010 == zero, 100 == neg */
 enum {
     FL_POS = 1 << 0, 
     FL_ZRO = 1 << 1,
     FL_NEG = 1 << 2
 };
 
+int main (int argc, const char* argv[]) {
+    if (argc < 2){
+        printf("lc3 [image-file1]: \n");
+        exit(2);
+    }
+
+    for (int j = 1; j < argc; ++j){
+        if (!read_image(argv[j])){
+            printf("failed to load image: %s\n", argv[j]);
+            exit(1);
+        }
+    }
+
+    reg[R_COND] = FL_ZRO;  /* initial cond flag is 0 */
+    enum {
+        PC_START = 0x3000 
+    };
+    reg[R_PC] = PC_START;
+
+    int running = 1;
+     while (running) {
+        /* Fetch */
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+
+        /* Decode then Execute */
+        switch(op){
+            case OP_ADD:
+                break;
+            case OP_AND:
+                break;
+            case OP_NOT:
+                break;
+            case OP_BR:
+                break;
+            case OP_JMP:
+                break;
+            case OP_JSR:
+                break;
+            case OP_LD:
+                break;
+            case OP_LDI:
+                break;
+            case OP_LDR:
+                break;
+            case OP_LEA:
+                break;
+            case OP_ST:
+                break;
+            case OP_STI:
+                break;
+            case OP_STR:
+                break;
+            case OP_TRAP:
+                break;
+            case OP_RES:
+            case OP_RTI:
+            default:
+                break;
+        }
+     }
+}
